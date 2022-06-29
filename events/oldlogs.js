ff1a43bc6bfd61a9916a5b204e8105ced7a5bbb0
Kailash Singh <105363681+singhkailash9@users.noreply.github.com>
2022-06-29 22:51:45 +0530

    Add files via upload

---
+++ events/oldlogs.js
@@ -0,0 +1,7 @@
+// if (cmd_name === 'ban' || cmd_name === 'kick'){
+		// 	message.channel.send('Cannot ban or kick');
+		// } else if (cmd_name === 'inspire'){
+		// 	message.channel.send('Touch some grass');
+		// } else if (cmd_name === 'quote'){
+		// 	getQuote().then(quote => message.channel.send(quote));
+		// }
